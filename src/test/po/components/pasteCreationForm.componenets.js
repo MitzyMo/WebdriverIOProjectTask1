d8cb4fcea5313d15b1c331637ceb24829594357c
Mitzy <mitzy.moreno@masabi.com>
2024-05-26 14:33:52 -0500

    adjusting to accept task 2

--- src/test/po/components/pasteCreationForm.componenets.js
+++ src/test/po/components/pasteCreationForm.componenets.js
@@ -2,6 +2,12 @@ class PasteCreationForm {
     get pasteTextArea() {
         return $('#postform-text');
     }
+    get syntaxHighlightingDropdown() {
+        return $('#select2-postform-format-container');
+    }
+    get bashSyntaxOption() {
+        return $('//li[contains(@class, "select2-results__option") and text()="Bash"]');
+    }
     get expirationDropdown() {
         return $('#select2-postform-expiration-container');
     }
@@ -16,6 +22,8 @@ class PasteCreationForm {
     }
     async createPaste(pasteText, pasteName, expirationTime) {
         await this.pasteTextArea.setValue(pasteText);
+        await this.syntaxHighlightingDropdown.click();
+        await this.bashSyntaxOption.click(); // Clicking on Bash syntax option
         await this.expirationDropdown.click();
         await this.tenMinutesOption.click();
         await this.postNameInput.setValue(pasteName);
--- src/test/specs/pastebinSkeleton.spec.js
+++ src/test/specs/pastebinSkeleton.spec.js
@@ -1,46 +1,56 @@
-describe('Pastebin New Paste', () => {
+describe('Pastebin New Paste Test Suite SKELETON', () => {
     beforeEach(async () => {
         await browser.url('https://pastebin.com');
     });
-
-    it('should create a new paste with specified attributes', async () => {
-        // Write in the paste text area
-        const pasteTextArea = await $('#postform-text');
-        await pasteTextArea.setValue("Hello from WebDriver");
-
-        // Select the expiration dropdown
-        const expirationDropdown = await $('#select2-postform-expiration-container');
-        await expirationDropdown.click();
-
-        // Choose the "10 Minutes" option
-        const tenMinutesOption = await $('//li[contains(@class, "select2-results__option") and text()="10 Minutes"]');
-        await tenMinutesOption.click();
-
-        // Enter the post name
-        const postNameInput = await $('#postform-name');
-        await postNameInput.setValue("helloweb");
-
-        // Press the "Create New Paste" button
-        const createPasteButton = await $('button.btn.-big');
-        await createPasteButton.click();
-
-        // Verify the new page URL contains a random ID
-        const currentUrl = await browser.getUrl();
-        expect(currentUrl).toMatch("https://pastebin.com/");
-
-        // Verify the paste name
-        const pasteName = await $('div.info-top h1');
-        const pasteNameText = await pasteName.getText();
-        expect(pasteNameText).toBe("helloweb");
-
-        // Verify the paste content
-        const pasteContent = await $('div.de1');
-        const pasteContentText = await pasteContent.getText();
-        expect(pasteContentText).toBe("Hello from WebDriver");
-
-        // Verify the expiration time
-        const expirationTime = await $('div[title="When this paste gets automatically deleted"]');
-        const expirationTimeText = await expirationTime.getText();
-        expect(expirationTimeText).toContain("10 MIN");
+ 
+    describe('Task 1', () => {
+        it('should create a new paste with specified attributes', async () => {
+            await $('#postform-text').setValue("Hello from WebDriver");
+            await $('#select2-postform-expiration-container').click();
+            await $('//li[contains(@class, "select2-results__option") and text()="10 Minutes"]').click();
+            await $('#postform-name').setValue("helloweb");
+            await $('button.btn.-big').click();
+ 
+            const currentUrl = await browser.getUrl();
+            expect(currentUrl).toMatch("https://pastebin.com/");
+ 
+            const pasteNameText = await $('div.info-top h1').getText();
+            expect(pasteNameText).toBe("helloweb");
+ 
+            const pasteContentText = await $('div.de1').getText();
+            expect(pasteContentText).toBe("Hello from WebDriver");
+ 
+            const expirationTimeText = await $('div[title="When this paste gets automatically deleted"]').getText();
+            expect(expirationTimeText).toContain("10 MIN");
+        });
     });
-});
+ 
+    describe('Task 2', () => {
+        it('should create a new paste with specified attributes and validate input', async () => {
+            await $('#postform-text').setValue("git config --global user.name  \"New Sheriff in Town\"\n" +
+                                                "git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\n" +
+                                                "git push origin master --force");
+            await $('#select2-postform-format-container').click();
+            await $('//li[contains(@class, "select2-results__option") and text()="Bash"]').click();
+            await $('#select2-postform-expiration-container').click();
+            await $('//li[contains(@class, "select2-results__option") and text()="10 Minutes"]').click();
+            await $('#postform-name').setValue("how to gain dominance among developers");
+            await $('button.btn.-big').click();
+ 
+            const currentUrl = await browser.getUrl();
+            expect(currentUrl).toMatch("https://pastebin.com/");
+ 
+            const pasteNameText = await $('div.info-top h1').getText();
+            expect(pasteNameText).toBe("how to gain dominance among developers");
+ 
+            const highlightedCodeText = await $('div.source.bash').getText();
+            expect(highlightedCodeText).toContain("git config");
+            expect(highlightedCodeText).toContain("git reset");
+            expect(highlightedCodeText).toContain("git push");
+ 
+            const pageTitle = await browser.getTitle();
+            expect(pageTitle).toBe("how to gain dominance among developers - Pastebin.com");
+        });
+    });
+ });
+ 
